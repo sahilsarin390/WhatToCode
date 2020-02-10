0412cc751b99f95431f59d507a81d480c3a1b6ce
Sahil Sareen <sahilsarin390@gmail.com>
2020-02-11 01:17:52 +0530

    first commit

---
+++ WhatToCode.js
@@ -0,0 +1,10 @@
+alert('Welcome to the almighty destination to guide you through your programming career!');
+function display_image(src, width, height, alt) { 
+    var img = document.createElement("img"); 
+img.src = src; 
+img.width = width; 
+img.height = height; 
+img.alt = alt; 
+document.body.appendChild(img);
+document.images['Giphy'].style.offset.top;
+}
